02b19af2acc376f290156dcf4cedd18bd9a3ef78
Rajesh Roy <rajeshroy402@gmail.com>
2024-03-24 15:40:03 +0530

    added conda

---
+++ install-conda.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#This is created by Rajesh Roy (rajeshroy402@gmail.com)
+
+echo "This repo is owned and managed by Rajesh Roy <rajeshroy402@gmail.com>"
+
+############################################################
+
+rm -rf ~/softwares_by_rajesh
+mkdir ~/softwares_by_rajesh
+
+cd ~/softwares_by_rajesh
+
+wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+
+bash ./Anaconda3-2024.02-1-Linux-x86_64.sh
+
+echo "We have installed conda in your system. Thanks."
+
+
